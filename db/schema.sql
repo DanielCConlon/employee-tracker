aba4be9e000b6b5cc5ba7ebb6ca1c2df94475d1d
Daniel Conlon <danielcconlon@gmail.com>
2022-07-09 10:50:04 -0400

    setting up employee table in seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -20,5 +20,6 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
-    manager_id INTEGER
+    manager_id INTEGER NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
